c2e411afab5fa9f544a550fd815ad37b85eeee52
fatima <fatimahassanhassab8@gmail.com>
2025-07-12 11:48:03 +0300

    add project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,67 +44,42 @@ export default class Projects extends Component {
           <div className="row">
             {/* Project 1 */}
             <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInLeft' : ''}`}
-            style={{animationDelay: '0.5s'}}
-            >
-              <div className="project-card">
-                <div className="project-card-caption">
-                  <h4>Pest Control</h4>
-                  <ul className="about-list text-left font-weight-normal">
-                    <li className="mb-4 text-dark">
-                      Creating a website for a pest control company
-                    </li>
-                    <li className="mb-4 text-dark">
-                      A static website interface using JavaScript, HTML, CSS, and Bootstrap.
-                    </li>
-                    <li className="mb-4 text-dark">
-                      Deploy the project on GitHub
-                    </li>
-                  </ul>
-                  <div className="project-links" >
-                    <a
-                      href="https://github.com/Fatimahs2002/Himaya2024"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn-primary btn-sm "
-                      style={{ background: '#695aa6',border:'none',boxShadow:'none' }}
-                    >
-                      GitHub
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* Project 2 */}
-            <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInRight' : ''}`}
             style={{animationDelay: '0.5s'}}>
-              <div className="project-card">
-                <div className="project-card-caption">
-                  <h4>Restaurant Website</h4>
-                  <ul className="about-list text-left font-weight-normal">
-                    <li className="mb-4 text-dark">
-                      Develop a restaurant website that allows users to manage menu items (CRUD operations)
-                    </li>
-                    <li className="mb-4 text-dark">
-                      A static website using both front-end and back-end technologies, including HTML, CSS, JavaScript, Bootstrap, SQL, and PHP.
-                    </li>
-                  </ul>
-                  <div className="project-links">
-                    <a
-                      href="https://github.com/Fatimahs2002/menu_violet"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn-primary btn-sm"
-                      style={{ background: '#695aa6',border:'none',boxShadow:'none' }}
-                    >
-                      GitHub
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-   {/* Project 3 */}
-   <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInLeft' : ''}`}
+  <div className="project-card">
+    <div className="project-card-caption">
+      <h4>An Employee Mangment System</h4>
+      <ul className="about-list text-left font-weight-normal">
+        <li className="mb-4 text-dark">
+          Developed a full-featured employee management system using React.js and 
+SQLite.
+        </li>
+        <li className="mb-4 text-dark">
+         Enabled HR to manage employees and track timesheets effectively.
+        </li>
+           <li className="mb-4 text-dark">
+       Integrated a calendar view to display all timesheets with start and end dates, 
+improving visibility and planning.
+        </li>
+      </ul>
+      <div className="project-links">
+        <a
+          href="https://github.com/Fatimahs2002/-employee-management-system.git"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary btn-sm"
+          style={{ background: '#695aa6', border: 'none', boxShadow: 'none' }}
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+            
+            
+            {/* Project 2 */}
+
+            <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInLeft' : ''}`}
             style={{animationDelay: '0.5s'}}>
   <div className="project-card">
     <div className="project-card-caption">
@@ -131,9 +106,10 @@ export default class Projects extends Component {
     </div>
   </div>
 </div>
-
-            {/* Project 4 */}
-            <div  className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInRight' : ''}`}
+            
+   {/* Project 3 */}
+   
+ <div  className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInRight' : ''}`}
             style={{animationDelay: '0.5s'}}>
   <div className="project-card">
     <div className="project-card-caption">
@@ -185,6 +161,70 @@ export default class Projects extends Component {
     </div>
   </div>
 </div>
+
+            {/* Project 4 */}
+           <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInRight' : ''}`}
+            style={{animationDelay: '0.5s'}}>
+              <div className="project-card">
+                <div className="project-card-caption">
+                  <h4>Restaurant Website</h4>
+                  <ul className="about-list text-left font-weight-normal">
+                    <li className="mb-4 text-dark">
+                      Develop a restaurant website that allows users to manage menu items (CRUD operations)
+                    </li>
+                    <li className="mb-4 text-dark">
+                      A static website using both front-end and back-end technologies, including HTML, CSS, JavaScript, Bootstrap, SQL, and PHP.
+                    </li>
+                  </ul>
+                  <div className="project-links">
+                    <a
+                      href="https://github.com/Fatimahs2002/menu_violet"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-primary btn-sm"
+                      style={{ background: '#695aa6',border:'none',boxShadow:'none' }}
+                    >
+                      GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+{/* Project 5 */}
+
+
+   <div className={`col-lg-6 col-md-6 mb-4 ${isVisible ? 'animate__animated animate__fadeInLeft' : ''}`}
+            style={{animationDelay: '0.5s'}}
+            >
+              <div className="project-card">
+                <div className="project-card-caption">
+                  <h4>Pest Control</h4>
+                  <ul className="about-list text-left font-weight-normal">
+                    <li className="mb-4 text-dark">
+                      Creating a website for a pest control company
+                    </li>
+                    <li className="mb-4 text-dark">
+                      A static website interface using JavaScript, HTML, CSS, and Bootstrap.
+                    </li>
+                    <li className="mb-4 text-dark">
+                      Deploy the project on GitHub
+                    </li>
+                  </ul>
+                  <div className="project-links" >
+                    <a
+                      href="https://github.com/Fatimahs2002/Himaya2024"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-primary btn-sm "
+                      style={{ background: '#695aa6',border:'none',boxShadow:'none' }}
+                    >
+                      GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
 
           </div>
         </div>
